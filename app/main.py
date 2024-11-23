9f40fda469766991449c78e722e28445eaa38c2b
Jost <jost@bebrian.Jost>
2024-11-23 11:15:19 -0500

    try not work

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ import os
 from app.utils import init_file
 import asyncio
 
-if __name__ == '__main__':
-    if not os.path.exists("uploads"):
-        os.makedirs("uploads")
+if not os.path.exists("uploads"):
+    os.makedirs("uploads")
 
 app = FastAPI()
+
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
@@ -18,13 +18,10 @@ async def create_upload_file(file: UploadFile = File(...)):
 
     if not os.path.isdir(os.path.join("uploads", file_name)):
         os.makedirs(f"uploads/{file_name}")
-    
+
     with open(f"uploads/{file_name}/{file.filename}", "wb") as f:
         f.write(await file.read())
-        
+
     asyncio.create_task(init_file(file_name_data, file.size))
-    
+
     return {"filename": file.filename}
-
-
-
